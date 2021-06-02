b560859a55cf211cebc190c853578cd74b8d68e5
Yeonathan Aizenberg <afula.aizenberg@gmail.com>
2021-06-02 19:45:14 +0300

    Soungs Set UP

--- front-end/src/Components/BookPage.js
+++ front-end/src/Components/BookPage.js
@@ -3,9 +3,20 @@ import { useState, useEffect } from 'react';
 import { useParams, withRouter } from 'react-router-dom';
 import { getBookById } from '../lib/booksApi';
 
-let soundFile = process.env.PUBLIC_URL + '/Soundtracks/theRoots-youGotMe.mp3';
+const anger = 'anger.mp3';
+const anticipation = 'anticipation.mp3';
+const disgust = 'disgust.mp3';
+const fear = 'fear.mp3';
+const joy = 'joy.mp3';
+const negative = 'negative.mp3';
+const positive = 'positive.mp3';
+const sadness = 'sadness.mp3';
+const surprise = 'surprise.mp3';
+const trust = 'trust.mp3';
 
 const BookPage = () => {
+    const [feeling, setFeeling] = useState(); 
+    let soundFile = process.env.PUBLIC_URL + '/Soundtracks/' + feeling;
     const params = useParams();
     const { bookId } = params;
     const [book, setBook] = useState();
@@ -25,10 +36,10 @@ const BookPage = () => {
     }, [bookId])
     
     const fetchData = async () => {
-    // const text = await getTextById(id)  Here we should get the text from database
     // const song = await getSongById(id)  Here we should get the song from database
-    setBookText(Lorem);
+    // const feel = await getFeeling  Here we should get the feeling from DS
     setBookSong(audioElement);
+    setFeeling(trust);
     };
 
     useEffect(() => {
@@ -38,10 +49,10 @@ const BookPage = () => {
     return (
         <div>
             <div 
-                onMouseEnter={() => audioElement.play()}
-                onMouseLeave={() => audioElement.pause()}
+                onMouseEnter={() => bookSong.play()}
+                onMouseLeave={() => bookSong.pause()}
                 >
-                    {!loading && book.text}
+                    {!loading && book.contents[0].text}
                     {loading && "Loading..."}
             </div>
         </div>
