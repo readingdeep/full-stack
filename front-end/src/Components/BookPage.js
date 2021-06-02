2de9c1d6282b816e3ca1cd7c96f9b5b7b356d37e
itailevi98 <you@example.com>
2021-06-02 19:38:20 +0300

    Updated book page

--- front-end/src/Components/BookPage.js
+++ front-end/src/Components/BookPage.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { useParams, withRouter } from 'react-router-dom';
-import { getBookById } from '../lib/booksApi';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams, withRouter } from "react-router-dom";
+import { getBookById } from "../lib/booksApi";
 
-let soundFile = process.env.PUBLIC_URL + '/Soundtracks/theRoots-youGotMe.mp3';
+let soundFile = process.env.PUBLIC_URL + "/Soundtracks/theRoots-youGotMe.mp3";
 
 const BookPage = () => {
     const params = useParams();
     const { bookId } = params;
     const [book, setBook] = useState();
+    const [bookPagesArray, setBookPagesArray] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
 
     const [loading, setLoading] = useState(true);
 
@@ -19,22 +21,47 @@ const BookPage = () => {
         async function fetchBook(bookId) {
             const fetchedBook = await getBookById(bookId);
             setBook(fetchedBook);
+            setBookPagesArray(fetchedBook.contents);
             setLoading(false);
         }
         fetchBook(bookId);
-    }, [bookId])
-    
+    }, [bookId]);
+
     return (
         <div>
-            <div 
-                onMouseEnter={() => audioElement.play()}
-                onMouseLeave={() => audioElement.pause()}
-                >
-                    {!loading && book.text}
-                    {loading && "Loading..."}
-            </div>
+            {loading && "Loading..."}
+            {!loading && (
+                <div>
+                    <h1>{book.title}</h1>
+                    <h2>{book.author}</h2>
+                    <div
+                        // onMouseEnter={() => audioElement.play()}
+                        // onMouseLeave={() => audioElement.pause()}
+                    >
+                        {!loading && (
+                            <div>{bookPagesArray[currentPage - 1].text}</div>
+                        )}
+                    </div>
+                    {currentPage > 1 && (
+                        <button
+                            type="button"
+                            onClick={() => setCurrentPage(currentPage - 1)}
+                        >
+                            Previous
+                        </button>
+                    )}
+                    {currentPage < bookPagesArray.length && (
+                        <button
+                            type="button"
+                            onClick={() => {console.log(currentPage); setCurrentPage(currentPage + 1);}}
+                        >
+                            Next
+                        </button>
+                    )}
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
 
 export default withRouter(BookPage);
